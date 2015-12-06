1604b42c1d316cd740ee1ee00aab45ca32508c97
Paweł Kryla <pawel.kryla@gmail.com>
2015-12-06 14:32:52 +0100

    Correct avg in student decorator

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,17 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+
+    @suma = 0.00
+    @liczba = subject_item_notes.where(subject_item_id: subject_item.id).count
+    @liczba == 0 ? @liczba = 1 : @liczba 
+
+    subject_item_notes.where(subject_item_id: subject_item.id).each do |item|
+      @suma = @suma + item.value
+    end
+
+    @average = @suma / @liczba 
+
+    return "#{'%.2f' % @average}"
   end
 end
